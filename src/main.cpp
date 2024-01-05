f19c731100910abc351cd5580eabc35057b1eb7b
JamesBremner <james@ravenspoint.com>
2024-01-05 12:26:38 -0500

    Code

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,116 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
 
-class cGUI : public cStarterGUI
+#include "GraphTheory.h"
+
+void read(
+    raven::graph::sGraphData &graphData,
+    int &capacity,
+    const std::string &fname)
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
-    {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+    std::ifstream ifs(fname);
+    if (!ifs.is_open())
+        throw std::runtime_error(
+            "Cannot open " + fname);
 
-        show();
-        run();
+    graphData.g.clear();
+    graphData.multiStart.clear();
+
+    std::string stype, sn1, sn2, scost, directed, same;
+    ifs >> stype;
+
+    while (ifs.good())
+    {
+        switch (stype[0])
+        {
+        case 'g':
+            if (graphData.g.vertexCount())
+                throw std::runtime_error(
+                    "g ( graph mode ) must be second line");
+            ifs >> directed >> same;
+            if (directed == "1")
+                graphData.g.directed();
+            break;
+        case 'l':
+            ifs >> sn1 >> sn2 >> scost;
+            graphData.g.add(sn1, sn2);
+            graphData.edgeWeight.push_back(atof(scost.c_str()));
+            if ((!graphData.g.isDirected()) && same == "1")
+            {
+                graphData.edgeWeight.push_back(atof(scost.c_str()));
+            }
+            break;
+        case 's':
+            ifs >> graphData.startName;
+            graphData.multiStart.push_back(graphData.startName);
+            break;
+        case 'e':
+            ifs >> graphData.endName;
+            break;
+        case 'c':
+            ifs >> capacity;
+        }
+
+        ifs >> stype;
+    }
+}
+int countChargeStops(
+    raven::graph::sGraphData &gd,
+    std::vector<int> &vPath,
+    int capacity)
+{
+    int tank = capacity;
+    int stops = 0;
+    for( auto it = vPath.begin(); it != vPath.end()-1; it++ )
+    {
+        int dist = gd.edgeWeight[gd.g.find(*it,*(it+1))];
+        if( dist < tank ) {
+            // next vertex reachable with charge remaining
+            tank -= dist;
+            continue;
+        }
+        // recharging stop
+        tank = capacity;
+        stops++;
+    }
+    return stops;
+}
+
+main(int argc, char *argv[])
+{
+    raven::graph::sGraphData gd;
+    int capacity;
+    
+    read(gd, capacity, argv[1]);
+
+    // find all paths between start and end
+    // in increasing length order
+    // uses Yen's algorithm
+
+    auto vpath = raven::graph::allPaths(gd);
+
+    bool feasible = false;
+    for (auto &p : vpath)
+    {
+        // check that longest hop in path can be covered with a full charge
+        if (*std::max_element(p.first.begin(), p.first.end()) > capacity)
+            continue;
+
+        // feasible path found
+        feasible = true;
+        for (int v : p.first)
+        {
+            std::cout << gd.g.userName(v) << " ";
+        }
+        std::cout << " with " 
+            << countChargeStops(gd, p.first, capacity)
+            << " stops\n";
+        break;
     }
 
-private:
-    wex::label &lb;
-};
-
-main()
-{
-    cGUI theGUI;
+    if (!feasible)
+        std::cout << "no feasible path\n";
+        
     return 0;
 }
